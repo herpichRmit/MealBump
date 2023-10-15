41ad1de80622b3039a680c8ea86fa788477f6fbf
herpichRmit <105478249+herpichRmit@users.noreply.github.com>
2023-10-15 16:26:19 +1100

    added sample property file for API key

--- a2s3407778/Views/Sheets/Subviews/SearchFoodView.swift
+++ a2s3407778/Views/Sheets/Subviews/SearchFoodView.swift
@@ -108,15 +108,16 @@ struct SearchFoodView: View {
 
 
 private var apiKey: String {
-  get {
-    // 1
-    guard let filePath = Bundle.main.path(forResource: "SpoonacularAPI-Info", ofType: "plist") else {
-      fatalError("Couldn't find file 'SpoonacularAPI-Info.plist'.")
-    }
-    // 2
-    let plist = NSDictionary(contentsOfFile: filePath)
-    guard let value = plist?.object(forKey: "API_KEY") as? String else {
-      fatalError("Couldn't find key 'API_KEY' in 'SpoonacularAPI-Info.plist'.")
+      get {
+        guard let filePath = Bundle.main.path(forResource: "SpoonacularAPI-Info", ofType: "plist") else {
+          fatalError("Couldn't find file 'SpoonacularAPI-Info.plist'.")
+        }
+        let plist = NSDictionary(contentsOfFile: filePath)
+        guard let value = plist?.object(forKey: "API_KEY") as? String else {
+          fatalError("Couldn't find key 'API_KEY' in 'SpoonacularAPI-Info.plist'.")
+        }
+      if (value.starts(with: "_")) {
+        fatalError("Register for a TMDB developer account and get an API key at https://developers.themoviedb.org/3/getting-started/introduction.")
     }
     return value
   }
